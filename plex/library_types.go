4e529628e0ce18658aa9a077083967c92284e308
clambin <christophe.lambin@gmail.com>
2023-11-18 16:16:45 +0100

    fix: timestamps in Library data structures

--- plex/library_types.go
+++ plex/library_types.go
@@ -19,7 +19,7 @@ type Library struct {
 	ScannedAt        Timestamp `json:"scannedAt"`
 	Content          bool      `json:"content"`
 	Directory        bool      `json:"directory"`
-	ContentChangedAt int       `json:"contentChangedAt"`
+	ContentChangedAt Timestamp `json:"contentChangedAt"`
 	Hidden           int       `json:"hidden"`
 	Location         []struct {
 		Id   int    `json:"id"`
@@ -28,30 +28,30 @@ type Library struct {
 }
 
 type Movie struct {
-	RatingKey             string  `json:"ratingKey"`
-	Key                   string  `json:"key"`
-	Guid                  string  `json:"guid"`
-	Studio                string  `json:"studio,omitempty"`
-	Type                  string  `json:"type"`
-	Title                 string  `json:"title"`
-	ContentRating         string  `json:"contentRating,omitempty"`
-	Summary               string  `json:"summary"`
-	Rating                float64 `json:"rating,omitempty"`
-	AudienceRating        float64 `json:"audienceRating,omitempty"`
-	ViewCount             int     `json:"viewCount,omitempty"`
-	LastViewedAt          int     `json:"lastViewedAt,omitempty"`
-	Year                  int     `json:"year,omitempty"`
-	Tagline               string  `json:"tagline,omitempty"`
-	Thumb                 string  `json:"thumb,omitempty"`
-	Art                   string  `json:"art,omitempty"`
-	Duration              int     `json:"duration"`
-	OriginallyAvailableAt string  `json:"originallyAvailableAt,omitempty"`
-	AddedAt               int     `json:"addedAt"`
-	UpdatedAt             int     `json:"updatedAt"`
-	AudienceRatingImage   string  `json:"audienceRatingImage,omitempty"`
-	PrimaryExtraKey       string  `json:"primaryExtraKey,omitempty"`
-	RatingImage           string  `json:"ratingImage,omitempty"`
-	Media                 []Media `json:"Media"`
+	RatingKey             string    `json:"ratingKey"`
+	Key                   string    `json:"key"`
+	Guid                  string    `json:"guid"`
+	Studio                string    `json:"studio,omitempty"`
+	Type                  string    `json:"type"`
+	Title                 string    `json:"title"`
+	ContentRating         string    `json:"contentRating,omitempty"`
+	Summary               string    `json:"summary"`
+	Rating                float64   `json:"rating,omitempty"`
+	AudienceRating        float64   `json:"audienceRating,omitempty"`
+	ViewCount             int       `json:"viewCount,omitempty"`
+	LastViewedAt          Timestamp `json:"lastViewedAt,omitempty"`
+	Year                  int       `json:"year,omitempty"`
+	Tagline               string    `json:"tagline,omitempty"`
+	Thumb                 string    `json:"thumb,omitempty"`
+	Art                   string    `json:"art,omitempty"`
+	Duration              int       `json:"duration"`
+	OriginallyAvailableAt string    `json:"originallyAvailableAt,omitempty"`
+	AddedAt               Timestamp `json:"addedAt"`
+	UpdatedAt             Timestamp `json:"updatedAt"`
+	AudienceRatingImage   string    `json:"audienceRatingImage,omitempty"`
+	PrimaryExtraKey       string    `json:"primaryExtraKey,omitempty"`
+	RatingImage           string    `json:"ratingImage,omitempty"`
+	Media                 []Media   `json:"Media"`
 	Genre                 []struct {
 		Tag string `json:"tag"`
 	} `json:"Genre,omitempty"`
@@ -109,31 +109,31 @@ type MediaPart struct {
 }
 
 type Show struct {
-	RatingKey             string  `json:"ratingKey"`
-	Key                   string  `json:"key"`
-	Guid                  string  `json:"guid"`
-	Studio                string  `json:"studio"`
-	Type                  string  `json:"type"`
-	Title                 string  `json:"title"`
-	ContentRating         string  `json:"contentRating"`
-	Summary               string  `json:"summary"`
-	Index                 int     `json:"index"`
-	AudienceRating        float64 `json:"audienceRating"`
-	ViewCount             int     `json:"viewCount,omitempty"`
-	LastViewedAt          int     `json:"lastViewedAt,omitempty"`
-	Year                  int     `json:"year"`
-	Thumb                 string  `json:"thumb"`
-	Art                   string  `json:"art"`
-	Theme                 string  `json:"theme,omitempty"`
-	Duration              int     `json:"duration"`
-	OriginallyAvailableAt string  `json:"originallyAvailableAt"`
-	LeafCount             int     `json:"leafCount"`
-	ViewedLeafCount       int     `json:"viewedLeafCount"`
-	ChildCount            int     `json:"childCount"`
-	AddedAt               int     `json:"addedAt"`
-	UpdatedAt             int     `json:"updatedAt"`
-	AudienceRatingImage   string  `json:"audienceRatingImage"`
-	PrimaryExtraKey       string  `json:"primaryExtraKey,omitempty"`
+	RatingKey             string    `json:"ratingKey"`
+	Key                   string    `json:"key"`
+	Guid                  string    `json:"guid"`
+	Studio                string    `json:"studio"`
+	Type                  string    `json:"type"`
+	Title                 string    `json:"title"`
+	ContentRating         string    `json:"contentRating"`
+	Summary               string    `json:"summary"`
+	Index                 int       `json:"index"`
+	AudienceRating        float64   `json:"audienceRating"`
+	ViewCount             int       `json:"viewCount,omitempty"`
+	LastViewedAt          Timestamp `json:"lastViewedAt,omitempty"`
+	Year                  int       `json:"year"`
+	Thumb                 string    `json:"thumb"`
+	Art                   string    `json:"art"`
+	Theme                 string    `json:"theme,omitempty"`
+	Duration              int       `json:"duration"`
+	OriginallyAvailableAt string    `json:"originallyAvailableAt"`
+	LeafCount             int       `json:"leafCount"`
+	ViewedLeafCount       int       `json:"viewedLeafCount"`
+	ChildCount            int       `json:"childCount"`
+	AddedAt               Timestamp `json:"addedAt"`
+	UpdatedAt             Timestamp `json:"updatedAt"`
+	AudienceRatingImage   string    `json:"audienceRatingImage"`
+	PrimaryExtraKey       string    `json:"primaryExtraKey,omitempty"`
 	Genre                 []struct {
 		Tag string `json:"tag"`
 	} `json:"Genre"`
@@ -149,39 +149,39 @@ type Show struct {
 }
 
 type Season struct {
-	RatingKey             string  `json:"ratingKey"`
-	Key                   string  `json:"key"`
-	ParentRatingKey       string  `json:"parentRatingKey"`
-	GrandparentRatingKey  string  `json:"grandparentRatingKey"`
-	Guid                  string  `json:"guid"`
-	ParentGuid            string  `json:"parentGuid"`
-	GrandparentGuid       string  `json:"grandparentGuid"`
-	Type                  string  `json:"type"`
-	Title                 string  `json:"title"`
-	GrandparentKey        string  `json:"grandparentKey"`
-	ParentKey             string  `json:"parentKey"`
-	GrandparentTitle      string  `json:"grandparentTitle"`
-	ParentTitle           string  `json:"parentTitle"`
-	ContentRating         string  `json:"contentRating"`
-	Summary               string  `json:"summary"`
-	Index                 int     `json:"index"`
-	ParentIndex           int     `json:"parentIndex"`
-	AudienceRating        float64 `json:"audienceRating"`
-	ViewCount             int     `json:"viewCount"`
-	LastViewedAt          int     `json:"lastViewedAt"`
-	Year                  int     `json:"year"`
-	Thumb                 string  `json:"thumb"`
-	Art                   string  `json:"art"`
-	ParentThumb           string  `json:"parentThumb"`
-	GrandparentThumb      string  `json:"grandparentThumb"`
-	GrandparentArt        string  `json:"grandparentArt"`
-	GrandparentTheme      string  `json:"grandparentTheme"`
-	Duration              int     `json:"duration"`
-	OriginallyAvailableAt string  `json:"originallyAvailableAt"`
-	AddedAt               int     `json:"addedAt"`
-	UpdatedAt             int     `json:"updatedAt"`
-	AudienceRatingImage   string  `json:"audienceRatingImage"`
-	Media                 []Media `json:"Media"`
+	RatingKey             string    `json:"ratingKey"`
+	Key                   string    `json:"key"`
+	ParentRatingKey       string    `json:"parentRatingKey"`
+	GrandparentRatingKey  string    `json:"grandparentRatingKey"`
+	Guid                  string    `json:"guid"`
+	ParentGuid            string    `json:"parentGuid"`
+	GrandparentGuid       string    `json:"grandparentGuid"`
+	Type                  string    `json:"type"`
+	Title                 string    `json:"title"`
+	GrandparentKey        string    `json:"grandparentKey"`
+	ParentKey             string    `json:"parentKey"`
+	GrandparentTitle      string    `json:"grandparentTitle"`
+	ParentTitle           string    `json:"parentTitle"`
+	ContentRating         string    `json:"contentRating"`
+	Summary               string    `json:"summary"`
+	Index                 int       `json:"index"`
+	ParentIndex           int       `json:"parentIndex"`
+	AudienceRating        float64   `json:"audienceRating"`
+	ViewCount             int       `json:"viewCount"`
+	LastViewedAt          Timestamp `json:"lastViewedAt"`
+	Year                  int       `json:"year"`
+	Thumb                 string    `json:"thumb"`
+	Art                   string    `json:"art"`
+	ParentThumb           string    `json:"parentThumb"`
+	GrandparentThumb      string    `json:"grandparentThumb"`
+	GrandparentArt        string    `json:"grandparentArt"`
+	GrandparentTheme      string    `json:"grandparentTheme"`
+	Duration              int       `json:"duration"`
+	OriginallyAvailableAt string    `json:"originallyAvailableAt"`
+	AddedAt               Timestamp `json:"addedAt"`
+	UpdatedAt             Timestamp `json:"updatedAt"`
+	AudienceRatingImage   string    `json:"audienceRatingImage"`
+	Media                 []Media   `json:"Media"`
 	Director              []struct {
 		Tag string `json:"tag"`
 	} `json:"Director"`
@@ -194,39 +194,39 @@ type Season struct {
 }
 
 type Episode struct {
-	RatingKey             string  `json:"ratingKey"`
-	Key                   string  `json:"key"`
-	ParentRatingKey       string  `json:"parentRatingKey"`
-	GrandparentRatingKey  string  `json:"grandparentRatingKey"`
-	Guid                  string  `json:"guid"`
-	ParentGuid            string  `json:"parentGuid"`
-	GrandparentGuid       string  `json:"grandparentGuid"`
-	Type                  string  `json:"type"`
-	Title                 string  `json:"title"`
-	GrandparentKey        string  `json:"grandparentKey"`
-	ParentKey             string  `json:"parentKey"`
-	GrandparentTitle      string  `json:"grandparentTitle"`
-	ParentTitle           string  `json:"parentTitle"`
-	ContentRating         string  `json:"contentRating"`
-	Summary               string  `json:"summary"`
-	Index                 int     `json:"index"`
-	ParentIndex           int     `json:"parentIndex"`
-	AudienceRating        float64 `json:"audienceRating"`
-	ViewCount             int     `json:"viewCount"`
-	LastViewedAt          int     `json:"lastViewedAt"`
-	Year                  int     `json:"year"`
-	Thumb                 string  `json:"thumb"`
-	Art                   string  `json:"art"`
-	ParentThumb           string  `json:"parentThumb"`
-	GrandparentThumb      string  `json:"grandparentThumb"`
-	GrandparentArt        string  `json:"grandparentArt"`
-	GrandparentTheme      string  `json:"grandparentTheme"`
-	Duration              int     `json:"duration"`
-	OriginallyAvailableAt string  `json:"originallyAvailableAt"`
-	AddedAt               int     `json:"addedAt"`
-	UpdatedAt             int     `json:"updatedAt"`
-	AudienceRatingImage   string  `json:"audienceRatingImage"`
-	Media                 []Media `json:"Media"`
+	RatingKey             string    `json:"ratingKey"`
+	Key                   string    `json:"key"`
+	ParentRatingKey       string    `json:"parentRatingKey"`
+	GrandparentRatingKey  string    `json:"grandparentRatingKey"`
+	Guid                  string    `json:"guid"`
+	ParentGuid            string    `json:"parentGuid"`
+	GrandparentGuid       string    `json:"grandparentGuid"`
+	Type                  string    `json:"type"`
+	Title                 string    `json:"title"`
+	GrandparentKey        string    `json:"grandparentKey"`
+	ParentKey             string    `json:"parentKey"`
+	GrandparentTitle      string    `json:"grandparentTitle"`
+	ParentTitle           string    `json:"parentTitle"`
+	ContentRating         string    `json:"contentRating"`
+	Summary               string    `json:"summary"`
+	Index                 int       `json:"index"`
+	ParentIndex           int       `json:"parentIndex"`
+	AudienceRating        float64   `json:"audienceRating"`
+	ViewCount             int       `json:"viewCount"`
+	LastViewedAt          Timestamp `json:"lastViewedAt"`
+	Year                  int       `json:"year"`
+	Thumb                 string    `json:"thumb"`
+	Art                   string    `json:"art"`
+	ParentThumb           string    `json:"parentThumb"`
+	GrandparentThumb      string    `json:"grandparentThumb"`
+	GrandparentArt        string    `json:"grandparentArt"`
+	GrandparentTheme      string    `json:"grandparentTheme"`
+	Duration              int       `json:"duration"`
+	OriginallyAvailableAt string    `json:"originallyAvailableAt"`
+	AddedAt               Timestamp `json:"addedAt"`
+	UpdatedAt             Timestamp `json:"updatedAt"`
+	AudienceRatingImage   string    `json:"audienceRatingImage"`
+	Media                 []Media   `json:"Media"`
 	Director              []struct {
 		Tag string `json:"tag"`
 	} `json:"Director"`
