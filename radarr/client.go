b819284b92537eff36afb20feb0e10f4d6c57937
Christophe Lambin <christophe.lambin@gmail.com>
2025-01-27 09:38:10 +0100

    build: pin peter-evans/create-pull-request v7

--- radarr/client.go
+++ radarr/client.go
@@ -1,3 +1,3 @@
 package radarr
 
-//go:generate oapi-codegen -config config.yaml https://raw.githubusercontent.com/Radarr/Radarr/refs/tags/v5.17.2.9580/src/Radarr.Api.V3/openapi.json
+//go:generate oapi-codegen -config config.yaml https://raw.githubusercontent.com/Radarr/Radarr/refs/tags/v5.17.2.9580fake/src/Radarr.Api.V3/openapi.json
