bcf04a8357e559554b2b8d2744a64d08837c568b
clambin <59534649+clambin@users.noreply.github.com>
2025-03-25 01:40:26 +0000

    [create-pull-request] automated change

--- radarr/client.go
+++ radarr/client.go
@@ -1,3 +1,3 @@
 package radarr
 
-//go:generate oapi-codegen -config config.yaml https://raw.githubusercontent.com/Radarr/Radarr/refs/tags/v5.19.3.9730/src/Radarr.Api.V3/openapi.json
+//go:generate oapi-codegen -config config.yaml https://raw.githubusercontent.com/Radarr/Radarr/refs/tags/v5.21.1.9799/src/Radarr.Api.V3/openapi.json
