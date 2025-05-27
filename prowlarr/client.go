a957105243cb62bb8dec8cb5e5f5da8c842684fb
clambin <59534649+clambin@users.noreply.github.com>
2025-05-27 01:46:52 +0000

    [create-pull-request] automated change

--- prowlarr/client.go
+++ prowlarr/client.go
@@ -1,3 +1,3 @@
 package prowlarr
 
-//go:generate oapi-codegen -config config.yaml https://raw.githubusercontent.com/Prowlarr/Prowlarr/refs/tags/v1.35.1.5034/src/Prowlarr.Api.V1/openapi.json
+//go:generate oapi-codegen -config config.yaml https://raw.githubusercontent.com/Prowlarr/Prowlarr/refs/tags/v1.36.3.5071/src/Prowlarr.Api.V1/openapi.json
