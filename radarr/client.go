d086d482a46a410054fa0cfd9557ed8d2c438c52
Christophe Lambin <christophe.lambin@gmail.com>
2025-07-02 16:33:07 +0200

    feat: bump Radarr to v5.26.2.10099

--- radarr/client.go
+++ radarr/client.go
@@ -1,3 +1,3 @@
 package radarr
 
-//go:generate go tool oapi-codegen -config config.yaml https://raw.githubusercontent.com/Radarr/Radarr/refs/tags/v5.25.0.10024/src/Radarr.Api.V3/openapi.json
+//go:generate go tool oapi-codegen -config config.yaml https://raw.githubusercontent.com/Radarr/Radarr/refs/tags/v5.26.2.10099/src/Radarr.Api.V3/openapi.json
