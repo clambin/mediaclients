375abf015afc70a4e7efa4d16aa8c69cd206e7f1
clambin <59534649+clambin@users.noreply.github.com>
2025-11-17 01:48:29 +0000

    [create-pull-request] automated change

--- sonarr/client.go
+++ sonarr/client.go
@@ -1,3 +1,3 @@
 package sonarr
 
-//go:generate  go tool oapi-codegen -config config.yaml https://raw.githubusercontent.com/Sonarr/Sonarr/refs/tags/v4.0.15.2941/src/Sonarr.Api.V3/openapi.json
+//go:generate  go tool oapi-codegen -config config.yaml https://raw.githubusercontent.com/Sonarr/Sonarr/refs/tags/v4.0.16.2944/src/Sonarr.Api.V3/openapi.json
