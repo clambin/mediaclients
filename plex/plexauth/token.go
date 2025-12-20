9e09e845285827568cfee5afff0f56a9b8234afa
Christophe Lambin <christophe.lambin@gmail.com>
2025-12-20 12:51:53 +0100

    protect against clock skew vs. plex.tv

--- plex/plexauth/token.go
+++ plex/plexauth/token.go
@@ -7,6 +7,13 @@ import (
 	"github.com/lestrrat-go/jwx/v3/jwt"
 )
 
+// plex.tv's time may drift from the client's time, meaning that if we verify the token,
+// the issue time ("iat") may be in the future. clockSkewTolerance indicated how far off
+// the time can be before we consider the token invalid.
+//
+// One minute is probably way too much.
+const clockSkewTolerance = time.Minute
+
 // Token represents a Plex authentication token. It can be either a legacy token (20-character string) or a JWT.
 // Note: JWTs currently cannot be used to access Plex Media Servers.
 type Token string
@@ -24,17 +31,19 @@ func (t Token) IsLegacy() bool {
 // IsJWT returns true if the token is a JWT.
 // Note: returns true even if the token is invalid (e.g. expired).
 func (t Token) IsJWT() bool {
-	_, err := jwt.Parse([]byte(t), jwt.WithVerify(false))
+	_, err := t.parseJWT()
 	return err == nil || errors.Is(err, jwt.TokenExpiredError())
 }
 
 // IsValid returns true if the token is valid.
 // Note: for JWT, the token is valid if it is not expired. The signature, if present, is not verified.
 func (t Token) IsValid() bool {
+	// parsing each time we validate the token isn't very efficient. currently not an issue as jwt tokens
+	// are not maintained (only needed to get the pms token, which is then cached), but may become an issue.
 	if t.IsLegacy() {
 		return true
 	}
-	tok, err := jwt.Parse([]byte(t), jwt.WithVerify(false))
+	tok, err := t.parseJWT()
 	if err != nil {
 		return false
 	}
@@ -43,4 +52,8 @@ func (t Token) IsValid() bool {
 		return false
 	}
 	return exp.After(time.Now())
+}
+
+func (t Token) parseJWT() (jwt.Token, error) {
+	return jwt.Parse([]byte(t), jwt.WithVerify(false), jwt.WithAcceptableSkew(clockSkewTolerance))
 }
