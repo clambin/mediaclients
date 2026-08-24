75bba966570d9a313a657e61dbb1fe123f7c1533
clambin <59534649+clambin@users.noreply.github.com>
2026-08-24 08:25:35 +0000

    [create-pull-request] automated change

--- radarr/client.go
+++ radarr/client.go
@@ -1,3 +1,3 @@
 package radarr
 
-//go:generate  go tool oapi-codegen -config config.yaml https://raw.githubusercontent.com/Radarr/Radarr/refs/tags/v6.1.1.10360/src/Radarr.Api.V3/openapi.json
+//go:generate  go tool oapi-codegen -config config.yaml https://raw.githubusercontent.com/Radarr/Radarr/refs/tags/v6.3.0.10514/src/Radarr.Api.V3/openapi.json
--- sonarr/client.go
+++ sonarr/client.go
@@ -1,3 +1,3 @@
 package sonarr
 
-//go:generate  go tool oapi-codegen -config config.yaml https://raw.githubusercontent.com/Sonarr/Sonarr/refs/tags/v4.0.17.2952/src/Sonarr.Api.V3/openapi.json
+//go:generate  go tool oapi-codegen -config config.yaml https://raw.githubusercontent.com/Sonarr/Sonarr/refs/tags/v4.0.19.2979/src/Sonarr.Api.V3/openapi.json
